b84b467b365eb1a5d3f35a6cdde1a54171f66a1c
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-10-14 10:55:30 +0800

    feat: create dynamic route for getting an employee

--- routes/employees.js
+++ routes/employees.js
@@ -14,6 +14,24 @@ router.get("/", async (_req, res) => {
 });
 
 /**
+ * Get a single employee by ID
+ */
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const request = await supabase
+    .from("employees")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (request.data === null) {
+    return res.status(404).json({ error: "Employee not found" });
+  }
+
+  res.json(request.data);
+});
+
+/**
  * Seed the database with 10 random employees
  */
 router.get("/seed", async (_req, res) => {
