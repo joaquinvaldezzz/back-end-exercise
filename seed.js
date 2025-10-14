9b7df55ac8894a222ec1e799473f5088ed8c3a6a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-10-14 11:20:06 +0800

    style: format code

--- seed.js
+++ seed.js
@@ -4,11 +4,7 @@ const employees = Array.from({ length: 100 }, () => ({
   first_name: faker.person.firstName(),
   middle_name: faker.person.middleName(),
   last_name: faker.person.lastName(),
-  employee_type: faker.helpers.arrayElement([
-    "FULL_TIME",
-    "PART_TIME",
-    "CONTRACTOR",
-  ]),
+  employee_type: faker.helpers.arrayElement(["FULL_TIME", "PART_TIME", "CONTRACTOR"]),
   salary: faker.number.int({ min: 30000, max: 120000 }),
 }));
 
