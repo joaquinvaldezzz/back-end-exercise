5f302ec3fb4393b5eb551eff9e52b0c646c2c8d3
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-10-15 08:47:30 +0800

    chore: install eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import path from 'node:path';
+import { FlatCompat } from '@eslint/eslintrc';
+import jsEslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+
+const compat = new FlatCompat({
+  baseDirectory: path.dirname(new URL(import.meta.url).pathname),
+});
+
+export default defineConfig([
+  { ignores: ['node_modules/**'] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    extends: ['js/recommended', eslintConfigPrettier],
+    languageOptions: { globals: globals.node },
+    plugins: { js: jsEslint, eslintPluginPrettier },
+  },
+  {
+    files: ['**/*.js'],
+    extends: [...compat.extends('airbnb-base')],
+    languageOptions: { sourceType: 'module' },
+  },
+]);
