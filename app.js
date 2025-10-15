271e0bd1b012aadbf41aea31e328eecb6d340a2c
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-10-15 11:07:09 +0800

    fix: correct path for static files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 app.use('/employees', employeesRouter);
 
