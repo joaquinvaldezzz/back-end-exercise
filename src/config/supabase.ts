26d0b1535163854d792cf65c1096c0d8b919963e
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-10-24 15:15:45 +0800

    chore: move database config

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.PROJECT_URL || '';
-const supabaseKey = process.env.API_KEY || '';
+const supabaseUrl = process.env.PROJECT_URL ?? '';
+const supabaseKey = process.env.API_KEY ?? '';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
